360b4287296495981f620244c28612526ce3282e
shahneilp <43914890+shahneilp@users.noreply.github.com>
2020-03-30 00:44:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ var tabulate = function (data,columns) {
 }
 
 d3.csv('data.csv',function (data) {
-	var columns = ['variable','aror','asd','maxdd']
+	var columns = ['Name','Height','Weight']
   tabulate(data,columns)
 })
